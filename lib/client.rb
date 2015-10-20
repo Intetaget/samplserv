05d405f54d8412a6c6bdfb0164c45ef73db15fb8
Mans Nilsson <intetaget>
2015-10-20 16:28:01 -0400

    client

--- lib/client.rb
+++ lib/client.rb
@@ -3,16 +3,44 @@ require "pry"
 
 class SamplParty
   include HTTParty
-  base_uri "http://localhost:4567"
+  base_uri "http://10.0.0.77:4567"
 
   def better(options={})
+
     SamplParty.post("/better", :body => options)
+  
+  end
+
+  def harder(options={})
+  	SamplParty.post("/harder", :body => options)
+  end
+
+  def faster(options={})
+  	SamplParty.post("/faster", :body => options)
+  end
+
+ def stronger(options={})
+  	SamplParty.post("/stronger", :body => options)
+  end
+   def beat
+  	SamplParty.post("/beat")
   end
 
   def stop
     SamplParty.get("/stop")
   end
 end
+server = SamplParty.new 
+server.beat
+server.harder
+sleep(0.5)
+server.better
+sleep(0.5)
+server.faster
+sleep (2)
+server.stronger
+sleep 10
+server.stop
 
-binding.pry
+#binding.pry
 
